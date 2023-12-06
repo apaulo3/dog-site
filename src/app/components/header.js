b8030ed153126d754b99fa33c7ee1b8aa645f704
CalDevC <mr.alexander5132@gmail.com>
2023-12-05 22:39:56 -0800

    Refreshed header design

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -2,10 +2,12 @@
 'use client';
 
 import styles from '../styles/header.module.scss';
-import Image from 'next/image';
+import commonStyles from '../styles/common.module.scss'
 import { Allison, Wix_Madefor_Text } from 'next/font/google';
 import prefix from '../prefix';
 import Link from 'next/link';
+import { Carousel } from "react-responsive-carousel";
+import Image from 'next/image';
 
 const allisonFont = Allison({
   subsets: ['latin'],
@@ -16,28 +18,71 @@ const wixFont = Wix_Madefor_Text({
   subsets: ['latin'],
 })
 
+let phrases = [
+  "Puppy",
+  "Companion",
+  "Best friend",
+  "Hunting buddy",
+  "Roommate",
+  "Loyal confidant",
+  "Playmate",
+  "Guardian angel",
+  "Cuddle buddy",
+  "Faithful sidekick",
+  "Adventurous partner",
+  "Devoted ally",
+  "Lifetime friend",
+  "Joyful spirit",
+  "Inseparable bond",
+]
+
 export default function Header() {
   return (
-    <div>
-      <div className={styles.header}>
-        <div className={styles.textContainer}>
-          <h1 className={`${styles.title} ${allisonFont.className}`}>PawPaws</h1>
-          <p className={`${styles.subTitle} ${wixFont.className}`}>Precious Pups</p>
+    <div className={styles.header}>
+      <div className={styles.heroInfoBlock}>
+        <div className={styles.title}>
+          <h1>Local breeders you can trust.</h1>
+        </div>
+        <div className={styles.subtext}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+          <div className={styles.dynamicText}>
+            <p>
+              {"Let us help you find your next"}
+            </p>
+            <Carousel
+              className={styles.carousel}
+              showArrows={false}
+              showIndicators={false}
+              infiniteLoop={true}
+              showThumbs={false}
+              dynamicHeight={false}
+              stopOnHover={false}
+              autoPlay={true}
+              axis="vertical"
+              statusFormatter={(current, total) => ``}
+            >
+              {phrases.map((phrase, index) => (
+                <p key={index} className={styles.heroWord}>{phrase}</p>
+              ))}
+            </Carousel>
+          </div>
         </div>
 
         <Link href="#puppies" className={styles.actionBtnLink} >
           <button className={styles.actionBtn}>view pups</button>
         </Link>
-
-        <div className={styles.imgOverlay} />
+      </div>
+      <div className={styles.imgContainer}>
         <Image
-          src={`${prefix}/landscape.jpeg`}
-          alt="Header Photo"
-          layout='responsive'
-          className={styles.img}
-          width={1200}
-          height={673}
+          src={`${prefix}/goldenHeadshot.png`}
+          alt="Captioned Image"
+          layout="responsive"
+          className={commonStyles.img}
           placeholder='empty'
+          width={100}
+          height={66.8}
         />
       </div>
     </div>
